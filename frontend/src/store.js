fe5cb590576b1e962a1319b1f7a7d35da6369f52
Farhana islam urmi <76726981+Farhanaurmi@users.noreply.github.com>
2021-05-31 18:27:30 +0600

    added project

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducer } from './reducer/prouductReducers'
 
-const reducer=combineReducers({})
+const reducer=combineReducers({
+    productList: productListReducer,
+})
 const initialState={}
 const middleware=[thunk]
 
-const store=createStore(reducer, initialState, composeWithDevTools(applyMiddleware(... middleware)))
+const store=createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
 
 export default store
